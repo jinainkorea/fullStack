2ead55fb762648975e0a52505b433d5ab3fc5c76
jinainkorea <kimjina1152@gmail.com>
2024-08-21 09:01:07 +0900

    앱 게시판 수정단계6

--- src/App.java
+++ src/App.java
@@ -38,36 +38,29 @@ public class App {
             } else if (input.contains("?")) {
                 String order = input.split("\\?")[0].trim();
                 String url_request;
-                int id;
 
                 // 예외처리
                 if (except_handling(order)) {
                        continue;
                 }
-                if ()
 
-                for (int i = 0; i < list.size(); i++) {
-                    Article article = (Article) list.get(i);
-                    if (article.getId() == id) {
+                int id = getId(list.size());
 
-                        if (order.equals("삭제")) {
-                            list.remove(i);
-                            System.out.println(article.getId() + "번 게시물이 삭제되었습니다.\n");
-                        } else if (order.equals("수정")) {
-                            System.out.println("제목(기존) : " + article.getTitle());
-                            System.out.printf("제목2 : ");
-                            String new_title = sc.nextLine();
-                            System.out.println("내용(기존) : " + article.getContent());
-                            System.out.printf("내용2 : ");
-                            String new_content = sc.nextLine();
+                if (order.equals("삭제")) {
+                    list.remove(i);
+                    System.out.println(article.getId() + "번 게시물이 삭제되었습니다.\n");
+                } else if (order.equals("수정")) {
+                    System.out.println("제목(기존) : " + article.getTitle());
+                    System.out.printf("제목2 : ");
+                    String new_title = sc.nextLine();
+                    System.out.println("내용(기존) : " + article.getContent());
+                    System.out.printf("내용2 : ");
+                    String new_content = sc.nextLine();
 
-                            Article article2 = new Article(id, new_title, new_content);
-                            list.remove(i);
-                            list.add(i, article2);
-                            System.out.println(article.getId() + "번 게시물 수정되었습니다. \n");
-                        }
-
-                    }
+                    Article article2 = new Article(id, new_title, new_content);
+                    list.remove(i);
+                    list.add(i, article2);
+                    System.out.println(article.getId() + "번 게시물 수정되었습니다. \n");
                 }
             } else {
                 System.out.println("유효한 명령어가 아닙니다.");
@@ -88,7 +81,7 @@ public class App {
             System.out.println("입력된 파라미터가 없습니다.");
             return true;
         }
-        //예외3: 입력된 파라미터가 옯바른지
+        //예외3: 입력된 파라미터가 올바른지
         if (url_request.split("=")[0].equals("id")) {
             //예외3-1: 입력된 파라미터가 id인 경우 값이 정수인지
             try {
@@ -101,16 +94,16 @@ public class App {
             System.out.println("삭제 명령어에 대한 올바른 파라미터가 아닙니다.");
             return true;
         }
+    }
 
-        public int getId(int listSize){
-            for (int i = 0; i < listSize; i++) {
-                Article article = (Article) list.get(i);
-                if (article.getId() == id) {
-                    return id;
-                }
+    public int getId(int listSize){
+        for (int i = 0; i < listSize; i++) {
+            Article article = (Article) list.get(i);
+            if (article.getId() == id) {
+                return id;
             }
-
         }
+        return null;
     }
 }
 
